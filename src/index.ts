a84508a6097f7907d47503efa0fe5976ba7524f8
atyahyde <krach.adt@gmail.com>
2026-02-03 17:12:40 +0700

    implement GET all subjects

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import express from "express";
+import cors from "cors";
+import subjectRouter from "./routes/subjects";
 
 const app = express();
 const PORT = 8000;
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "DELETE"],
+    credentials: true,
+  }),
+);
+
 // JSON middleware
 app.use(express.json());
+
+app.use("/api/subjects", subjectRouter);
 
 // Start server
 app.listen(PORT, () => {
